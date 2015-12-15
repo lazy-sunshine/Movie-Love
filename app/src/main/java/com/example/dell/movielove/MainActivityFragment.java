191fd9d345afba74a8550048d03f4de034cf1015
lazy-sunshine <kirti7756@gmail.com>
2015-12-15 11:56:35 +0530

    using Handler to communicate withAsync Task

--- app/src/main/java/com/example/dell/movielove/MainActivityFragment.java
+++ app/src/main/java/com/example/dell/movielove/MainActivityFragment.java
@@ -1,9 +1,9 @@
 package com.example.dell.movielove;
 
 import android.content.Intent;
-import android.net.Uri;
-import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -16,16 +16,6 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -34,15 +24,17 @@ import java.util.Arrays;
  */
 public class MainActivityFragment extends Fragment implements PhotoAlbumAdapter.OnItemClickListener {
 
+    String choice1;
+    String[] resultStrs1;
+    String[] movie_title1;
+    String[] movie_plot1;
+    String[] user_rating1;
+    String[] release_date1;
     private RecyclerView recyclerView;
     private RecyclerView.LayoutManager layoutManager;
     PhotoAlbumAdapter mAdapter;
- String choice;
-    String[] resultStrs;
-    String[] movie_title;
-    String[] movie_plot;
-    String[] user_rating;
-    String[] release_date;
+    Handler h;
+
     public MainActivityFragment() {
     }
 
@@ -96,12 +88,12 @@ public class MainActivityFragment extends Fragment implements PhotoAlbumAdapter.
 
 
         registerForContextMenu(recyclerView);
-            updateMovie("vote_count.desc");
+        updateMovie("vote_count.desc");
         return rootView;
     }
 
     private void updateMovie(String c) {
-        new FetchMoviePoster().execute(c);
+        new FetchMoviePoster(new MyHandler()).execute(c);
     }
 
     @Override
@@ -115,138 +107,31 @@ public class MainActivityFragment extends Fragment implements PhotoAlbumAdapter.
     @Override
     public void onItemClick(View view, int position) {
         Log.v("Actual position", Integer.toString(position));
-       Intent detail= new Intent(getActivity(),DetailActivity.class);
+        Intent detail= new Intent(getActivity(),DetailActivity.class);
         detail.putExtra("position",position);
-        detail.putExtra("poster", resultStrs);
+        detail.putExtra("poster", resultStrs1);
 
-        detail.putExtra("title", movie_title);
-        detail.putExtra("plot", movie_plot);
-        detail.putExtra("user_rating", user_rating);
-        detail.putExtra("release", release_date);
+        detail.putExtra("title", movie_title1);
+        detail.putExtra("plot", movie_plot1);
+        detail.putExtra("user_rating", user_rating1);
+        detail.putExtra("release", release_date1);
         startActivity(detail);
     }
 
 
-
-    public class FetchMoviePoster extends AsyncTask<String, Void, String[]> {
-        private final String LOG_TAG = FetchMoviePoster.class.getSimpleName();
-
-
+    private class MyHandler extends Handler {
         @Override
-        protected String[] doInBackground(String... code) {
-
-            HttpURLConnection urlConnection = null;
-            BufferedReader reader = null;
-            String[] image_poster = new String[0];
-            String API = "api_key";
-            String SORT="sort_by";
-
-            choice=code[0];
-            String forecastJsonStr = null;
-
-            try {
-                Uri fetch_url = Uri.parse("http://api.themoviedb.org/3/discover/movie?");
-                Uri builder = fetch_url.buildUpon().
-                        appendQueryParameter(SORT,choice)
-                        .appendQueryParameter(API, BuildConfig.THE_MOVIE_DB_API_KEY).build();
-                URL url = new URL(builder.toString());
-
-
-
-                urlConnection = (HttpURLConnection) url.openConnection();
-                urlConnection.setRequestMethod("GET");
-                urlConnection.connect();
-
-                InputStream inputStream = urlConnection.getInputStream();
-                StringBuffer buffer = new StringBuffer();
-                if (inputStream == null) {
-                    // Nothing to do.
-                    return null;
-                }
-                reader = new BufferedReader(new InputStreamReader(inputStream));
-
-                String line;
-                while ((line = reader.readLine()) != null) {
-                    buffer.append(line + "/n");
-                }
-
-                if (buffer.length() == 0) {
-                    return null;
-                }
-                forecastJsonStr = buffer.toString();
-
-
-                try {
-                    image_poster = getUrlForImage(forecastJsonStr);
-
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-
-
-            } catch (IOException e) {
-                Log.e(LOG_TAG, "Error ", e);
-                // If the code didn't successfully get the weather data, there's no point in attempting
-                // to parse it.
-                forecastJsonStr = null;
-            } finally {
-                if (urlConnection != null) {
-                    urlConnection.disconnect();
-                }
-                if (reader != null) {
-                    try {
-                        reader.close();
-                    } catch (final IOException e) {
-                        Log.e(LOG_TAG, "Error closing stream", e);
-                    }
-                }
-            }
-            return image_poster;
-        }
-
-        private String[] getUrlForImage(String forecastJsonStr) throws JSONException {
-            final String OWM_POSTER = "results";
-            final String FILE_PATH = "poster_path";
-            final String ORIGINAL_TITLE="original_title";
-            final String OVERVIEW="overview";
-            final String User_RATING="vote_average";
-            final String RELEASE="release_date";
-
-            JSONObject forecastJson = new JSONObject(forecastJsonStr);
-            JSONArray movieArray = forecastJson.getJSONArray(OWM_POSTER);
-             resultStrs = new String[movieArray.length()];
-            movie_title=new String[movieArray.length()];
-            movie_plot=new String[movieArray.length()];
-            user_rating=new String[movieArray.length()];
-            release_date=new String[movieArray.length()];
-            for (int i = 0; i < movieArray.length(); i++) {
-
-                JSONObject movie = movieArray.getJSONObject(i);
-
-                String file_name = movie.getString(FILE_PATH);
-                String title=movie.getString(ORIGINAL_TITLE);
-                String plot=movie.getString(OVERVIEW);
-                String userRating=movie.getString(User_RATING);
-                String release=movie.getString(RELEASE);
-                                resultStrs[i] = "http://image.tmdb.org/t/p/w185/" +file_name;
-              movie_title[i]=title;
-                movie_plot[i]=plot;
-                user_rating[i]= userRating;
-                release_date[i]=release;
-            }
-
-            return resultStrs;
-        }
-
-        @Override
-        protected void onPostExecute(String[] resultStrs) {
-            if (resultStrs != null) {
-                mAdapter = new PhotoAlbumAdapter(new ArrayList<>(Arrays.asList(resultStrs)), getContext());
-                recyclerView.setAdapter(mAdapter);
-                mAdapter.SetOnItemClickListener(MainActivityFragment.this);
-
-
-            }
+        public void handleMessage(Message msg) {
+            Bundle c=msg.getData();
+            c.setClassLoader(ClassLoader.getSystemClassLoader());
+            resultStrs1=c.getStringArray("result");
+             movie_title1=c.getStringArray("movie_title");
+            movie_plot1=c.getStringArray("movie_plot");
+            user_rating1=c.getStringArray("user_rating");
+            release_date1=c.getStringArray("release date");
+            mAdapter = new PhotoAlbumAdapter(new ArrayList<>(Arrays.asList(resultStrs1)), getContext());
+            recyclerView.setAdapter(mAdapter);
+            mAdapter.SetOnItemClickListener(MainActivityFragment.this);
 
         }
     }
